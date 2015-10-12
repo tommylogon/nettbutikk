33808e0874097770316180e1f12b5d5f5275f44b
tommylogon <tommy.slaatbraaten@hotmail.com>
2015-10-12 11:10:22 +0200

    stuff an such

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 	window.onload = start;
 	
 	function start(){
-		document.getElementById("kjop").onclick = addToCheckout;
+		document.getElementById("btnKjop").onclick = addToCheckout;
+		document.getElementById("checkout").onclick = payAndDelivery;
 	}
+	var prdnmr = document.getElementById("btnKjop").value;
 	function addToCheckout(){
 			alert("du har kjøpt en pc");
+			
 			window.location = "../../checkout/checkout.html"
+			valuesToCheckout();
+	}
+	function valuesToCheckout(){
+		document.getElementById("prdNum1").value= prdnmr;
 	}
