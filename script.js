300914a6bb0a6901329a8907ba206ed563254ee3
tommylogon <tommy.slaatbraaten@hotmail.com>
2015-10-12 12:46:32 +0200

    jaavaaa

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 	window.onload = start;
 	
 	function start(){
-		if(window.location == "acer.html"){
-			document.getElementById("btnKjop").onclick = addToCheckout;
-			
-		}
+				document.getElementById("btnKjop").onclick = addToCheckout;
 				document.getElementById("checkout").onclick = payAndDelivery;
-			
+				document.getElementById("selProd").onchange = addProdNum;
 
 	}
 	function addToCheckout(){
 			prodnum= document.getElementById("btnKjop").value;
-			alert("du har lagt til produkt " + prodnum);
-			
-			
-			valuesToCheckout();
+			alert("du har valgt produkt " + prodnum+ ", legg til dette nummeret på neste side");
+			window.location.assign("../../checkout/checkout.html");
 	}
-	function valuesToCheckout(){
-		document.getElementById("prdNum1").value= prdnmr;
-		window.location = "../../checkout/checkout.html";
+	function addProdNum(){
+		var prodnum = document.getElementById("selProd").value;
+		document.getElementById("prdNum").innerHTML = prodnum;
 	}
 	function payAndDelivery(){
-		alert("du har kjøpt " + document.getElementById("prdNum1").value + " og den forventes levert imorgen");
+		var produkt= document.getElementById("selProd").value;
+		alert("du har kjøpt " + produkt + " og den forventes levert imorgen");
 	}
